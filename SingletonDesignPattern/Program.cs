1c765ec0713d2ab8f4e9fd1c6612ede7e908b75f
emir57 <emir.gurbuz06@hotmail.com>
2022-01-23 15:33:49 +0300

    Singleton Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            Console.WriteLine(s1.Id);
+            Console.WriteLine(s2.Id);
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    class Singleton
+    {
+        private static Singleton _instance { get; set; }
+        public Guid Id { get; }
+        private static object _lockObject = new object();
+        private Singleton(Guid id)
+        {
+            Id = id;
+        }
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lockObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton(Guid.NewGuid());
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
