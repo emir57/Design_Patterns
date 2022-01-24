2e26e83674dfaa85fb700c563a73b967fa215a81
emir57 <emir.gurbuz06@hotmail.com>
2022-01-24 15:20:26 +0300

    AdapterDesignPattern

---
+++ AdapterDesignPattern/IErrorModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern
+{
+    public interface IErrorModel
+    {
+        int ErrorNumber { get; set; }
+        string Description { get; set; }
+        void SendMail();
+    }
+    public class DbError : IErrorModel
+    {
+        private int _errorNumber;
+        private string _description;
+        public int ErrorNumber
+        {
+            get
+            {
+                return _errorNumber;
+            }
+            set
+            {
+                _errorNumber = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("{0} - {1} => DbError is send",_errorNumber,_description);
+        }
+    }
+    public class ServerError : IErrorModel
+    {
+        private int _errorNumber;
+        private string _description;
+        public int ErrorNumber
+        {
+            get
+            {
+                return _errorNumber;
+            }
+            set
+            {
+                _errorNumber = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("{0} - {1} => ServerError is send", _errorNumber, _description);
+        }
+    }
+
+    public class FaxAdapter : IErrorModel
+    {
+        private Fax _fax;
+        public FaxAdapter(Fax fax)
+        {
+            _fax = fax;
+        }
+        public int ErrorNumber 
+        { 
+            get
+            {
+                return _fax.FaxErrorCode;
+            }
+            set
+            {
+                _fax.FaxErrorCode = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return _fax.ErrorDescription;
+            }
+            set
+            {
+                _fax.ErrorDescription = value;
+            }
+        }
+        public void SendMail()
+        {
+            Console.WriteLine("{0} {1} fax exception",ErrorNumber,Description);
+        }
+    }
+
+    public class Fax
+    {
+        public int FaxErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+
+        void Send()
+        {
+
+        }
+        void Get()
+        {
+
+        }
+    }
+
+}
---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fax fax = new Fax 
+                { FaxErrorCode = 4000, ErrorDescription = "server is not found" };
+            IErrorModel[] errors =
+            {
+                new DbError{ErrorNumber=1000,Description="Connection is timeout"},
+                new DbError{ErrorNumber=1001,Description="Query Exception"},
+                new ServerError{ErrorNumber=3000,Description="UnAuthorized"},
+                new ServerError{ErrorNumber=3004,Description="Server is notfound"},
+                new FaxAdapter(fax)
+            };
+            foreach (var error in errors)
+            {
+                error.SendMail();
+            }
+        }
+    }
+}
