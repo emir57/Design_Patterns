52c1b69eb1bea3954f04209d377bda037f262a39
emir57 <emir.gurbuz06@hotmail.com>
2022-01-26 15:59:43 +0300

    Complete ProxyDesignPattern

--- ProxyDesignPattern/Program.cs
+++ ProxyDesignPattern/Program.cs
@@ -2,11 +2,53 @@
 
 namespace ProxyDesignPattern
 {
+    //Subject
+    interface IImageGenerator
+    {
+        void ShowImage();
+    }
+    class ImageGenerator : IImageGenerator
+    {
+        private string _fullPath;
+        public ImageGenerator(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+        public void ShowImage()
+        {
+            Console.WriteLine("{0} image is showing",_fullPath);
+        }
+    }
+
+    class ImageGeneratorProxy : IImageGenerator
+    {
+        private ImageGenerator _imageGenerator;
+        private string _fullPath;
+        public ImageGeneratorProxy(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+        public void ShowImage()
+        {
+            if(_imageGenerator == null)
+            {
+                _imageGenerator = new ImageGenerator(_fullPath);
+            }
+            _imageGenerator.ShowImage();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            ImageGeneratorProxy proxy1 = new ImageGeneratorProxy("c://image1.jpg");
+            ImageGeneratorProxy proxy2 = new ImageGeneratorProxy("c://image2.jpg");
+
+            proxy1.ShowImage();
+            proxy2.ShowImage();
+            proxy1.ShowImage();
+
         }
     }
 }
