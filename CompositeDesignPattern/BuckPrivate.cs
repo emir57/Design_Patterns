77939188dbff6c90dfbc39fd0def56b3a2ab4ad9
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 16:03:54 +0300

    Refactoring

--- CompositeDesignPattern/BuckPrivate.cs
+++ CompositeDesignPattern/BuckPrivate.cs
@@ -6,16 +6,8 @@
         {
 
         }
-        public override void AddSoldier(Soldier soldier)
-        {
-            throw new System.NotImplementedException();
-        }
 
-        public override void ExecuteSoldier() => System.Console.WriteLine($"{_rank} - {_name}");
-
-        public override void RemoveSoldier(Soldier soldier)
-        {
-            throw new System.NotImplementedException();
-        }
+        public override void ExecuteSoldier() 
+            => System.Console.WriteLine($"{_rank} - {_name}");
     }
 }
