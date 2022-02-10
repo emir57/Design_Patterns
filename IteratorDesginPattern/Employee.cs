5b8a7a998ca958e7a5334602e98bca7dc5d7b98a
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 14:38:18 +0300

    Employee

---
+++ IteratorDesginPattern/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace IteratorDesignPattern
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public int FirtsName { get; set; }
+        public int LastName { get; set; }
+    }
+}
--- IteratorDesginPattern/Program.cs
+++ IteratorDesginPattern/Program.cs
@@ -6,7 +6,7 @@ namespace IteratorDesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
