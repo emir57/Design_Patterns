4bb6416014bd15739dcea2efd040a6b0aa76f6f8
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 14:46:56 +0300

    EmployeeAggregate and EmployeeIterator

---
+++ IteratorDesginPattern/EmployeeAggregate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorDesignPattern
+{
+    class EmployeeAggregate : IAggregate
+    {
+        List<Employee> List = new List<Employee>();
+        public void Add(Employee employee) => List.Add(employee);
+        public Employee getItem(int index) => List[index];
+        public int Count { get => List.Count; }
+        public IIterator CreateIterator() => new EmployeeIterator(this);
+    }
+}
---
+++ IteratorDesginPattern/EmployeeIterator.cs
@@ -0,0 +1,30 @@
+﻿namespace IteratorDesignPattern
+{
+    class EmployeeIterator : IIterator
+    {
+        EmployeeAggregate _aggregate;
+        int currentIndex;
+        public EmployeeIterator(EmployeeAggregate aggregate)
+        {
+            _aggregate = aggregate;
+        }
+        public Employee CurrentItem()
+        {
+            return _aggregate.getItem(currentIndex);
+        }
+
+        public bool HasItem()
+        {
+            if (currentIndex < _aggregate.Count)
+                return true;
+            return false;
+        }
+
+        public Employee NextItem()
+        {
+            if (HasItem())
+                return _aggregate.getItem(currentIndex++);
+            return new Employee();
+        }
+    }
+}
