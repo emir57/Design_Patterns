36630ae2b679aa5a2d732c776587487042ff1d30
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 14:50:26 +0300

    Complete Iterator

--- IteratorDesginPattern/Program.cs
+++ IteratorDesginPattern/Program.cs
@@ -7,7 +7,16 @@ namespace IteratorDesignPattern
         static void Main(string[] args)
         {
             EmployeeAggregate aggregate = new EmployeeAggregate();
-            aggregate.Add(new Employee { Id = 1, FirtsName = "Emir", LastName = "Gurbuz" });
+            aggregate.Add(new Employee { Id = 1, FirstName = "Emir", LastName = "Gurbuz" });
+            aggregate.Add(new Employee { Id = 2, FirstName = "Kadir", LastName = "Cakmak" });
+            aggregate.Add(new Employee { Id = 3, FirstName = "Süleyman", LastName = "Yaz" });
+
+            IIterator iterator = aggregate.CreateIterator();
+            while (iterator.HasItem())
+            {
+                Console.WriteLine("{0} - {1} {2}",iterator.CurrentItem().Id,iterator.CurrentItem().FirstName,iterator.CurrentItem().LastName);
+                iterator.NextItem();
+            }
         }
     }
 }
