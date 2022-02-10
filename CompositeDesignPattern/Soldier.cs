ab4c3609738b810a534a5fd7030f0327b3f34da8
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 15:54:54 +0300

    Soldier

---
+++ CompositeDesignPattern/Soldier.cs
@@ -0,0 +1,16 @@
+﻿namespace CompositeDesignPattern
+{
+    abstract class Soldier
+    {
+        private _name;
+        private Rank _rank;
+        public Soldier(string name,Rank rank)
+        {
+            _name = name;
+            _rank = rank;
+        }
+        public abstract void AddSoldier(Soldier soldier);
+        public abstract void RemoveSoldier(Soldier soldier);
+        public abstract void ExecuteSoldier();
+    }
+}
