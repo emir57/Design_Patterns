630a1bd3cf31989e125ff4c83629be35f1b0deed
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 15:55:22 +0300

    Rank

---
+++ CompositeDesignPattern/Rank.cs
@@ -0,0 +1,12 @@
+﻿namespace CompositeDesignPattern
+{
+    enum Rank
+    {
+        General, //General
+        Colonel, //Albay
+        LieutenantColonel, //Yarbay
+        Major, //Binbaşı
+        Captain, //Yüzbaşı
+        Lieutenant //Teğmen
+    }
+}
