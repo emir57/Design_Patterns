0d3a29432d44d9472e5a23a14444451f0582b6f5
emir57 <emir.gurbuz06@hotmail.com>
2022-02-10 16:01:43 +0300

    BuckPrivate

---
+++ CompositeDesignPattern/BuckPrivate.cs
@@ -0,0 +1,21 @@
+﻿namespace CompositeDesignPattern
+{
+    class BuckPrivate : Soldier
+    {
+        public BuckPrivate(string name,Rank rank):base(name,rank)
+        {
+
+        }
+        public override void AddSoldier(Soldier soldier)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ExecuteSoldier() => System.Console.WriteLine($"{_rank} - {_name}");
+
+        public override void RemoveSoldier(Soldier soldier)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
