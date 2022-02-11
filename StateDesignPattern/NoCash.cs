3be692f10e3d4f3c8b242b9e5876a4d5a6bca3ad
emir57 <emir.gurbuz06@hotmail.com>
2022-02-11 16:00:57 +0300

    No Cash

---
+++ StateDesignPattern/NoCash.cs
@@ -0,0 +1,25 @@
+﻿namespace StateDesignPattern
+{
+    class NoCash : AtmState
+    {
+        public override void EjectCard(AtmMachine context)
+        {
+            System.Console.WriteLine("Para Yok");
+        }
+
+        public override void InsertCard(AtmMachine context)
+        {
+            System.Console.WriteLine("Para Yok");
+        }
+
+        public override void InsertPin(int pin, AtmMachine context)
+        {
+            System.Console.WriteLine("Para Yok");
+        }
+
+        public override void RequestCash(int cashToWithDraw, AtmMachine context)
+        {
+            System.Console.WriteLine("Para Yok");
+        }
+    }
+}
