44393e9516df4e1ba4c526db675ee2de52b50fa7
emir57 <emir.gurbuz06@hotmail.com>
2022-02-11 15:47:14 +0300

    AtmState

---
+++ StateDesignPattern/AtmState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDesignPattern
+{
+    abstract class AtmState
+    {
+        public abstract void InsertCard(AtmMachine contetx);
+        public abstract void EjectCard(AtmMachine contetx);
+        public abstract void InsertPin(int pin,AtmMachine contetx);
+        public abstract void RequestCash(int cashToWithDraw,AtmMachine contetx);
+    }
+}
