d29316112d211ad9b261b0c34ed75425b0841532
emir57 <emir.gurbuz06@hotmail.com>
2022-02-11 15:54:16 +0300

    NoCard

---
+++ StateDesignPattern/NoCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StateDesignPattern
+{
+    class NoCard : AtmState
+    {
+        public override void EjectCard(AtmMachine context)
+        {
+            Console.WriteLine("Kart Takılı Değil");
+        }
+
+        public override void InsertCard(AtmMachine context)
+        {
+            Console.WriteLine("Lütfen pin giriniz");
+            context = new HasCard();
+        }
+
+        public override void InsertPin(int pin, AtmMachine context)
+        {
+            Console.WriteLine("Kart Takılı Değil");
+        }
+
+        public override void RequestCash(int cashToWithDraw, AtmMachine context)
+        {
+            Console.WriteLine("Kart Takılı Değil");
+        }
+    }
+}
