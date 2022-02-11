f7d9ef563a29ada26def0ead8365afb21e58ce97
emir57 <emir.gurbuz06@hotmail.com>
2022-02-11 15:51:51 +0300

    AtmMachine and refactoring

---
+++ StateDesignPattern/AtmMachine.cs
@@ -0,0 +1,15 @@
+﻿namespace StateDesignPattern
+{
+    class AtmMachine
+    {
+        AtmState state = new NoCard();
+        public AtmState State { set { state = value; } }
+        public int CashToMachine { get; set; } = 2000;
+        public bool CorrectPinEntered { get; set; } = false;
+        public void InsertCard() => state.InsertCard(this);
+        public void EjectCard() => state.EjectCard(this);
+        public void InsertPin(int pin) => state.InsertPin(pin, this);
+        public void RequestCash(int cashToWithDraw) => state.RequestCash(cashToWithDraw, this);
+
+    }
+}
--- StateDesignPattern/AtmState.cs
+++ StateDesignPattern/AtmState.cs
@@ -8,9 +8,9 @@ namespace StateDesignPattern
 {
     abstract class AtmState
     {
-        public abstract void InsertCard(AtmMachine contetx);
-        public abstract void EjectCard(AtmMachine contetx);
-        public abstract void InsertPin(int pin,AtmMachine contetx);
-        public abstract void RequestCash(int cashToWithDraw,AtmMachine contetx);
+        public abstract void InsertCard(AtmMachine context);
+        public abstract void EjectCard(AtmMachine context);
+        public abstract void InsertPin(int pin,AtmMachine context);
+        public abstract void RequestCash(int cashToWithDraw,AtmMachine context);
     }
 }
